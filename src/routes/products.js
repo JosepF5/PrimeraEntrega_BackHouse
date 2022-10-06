b6fbb6d98ac303581496549eb5c23804c8b6ec29
Josep Palomino <jfpalomino.22@gmail.com>
2022-10-06 01:29:24 -0500

    prueba :p

---
+++ src/routes/products.js
@@ -0,0 +1,74 @@
+const Contenedor = require("../contenedor.js");
+const Producto = require("../model/product");
+const nuevo = new Contenedor("./productos.json");
+
+module.exports = function (router) {
+
+  /* RENDERIZAR PAGINA DE PRODUCTOS 
+  router.get("/", async (req, res) => {
+    const productos = await nuevo.getAll();
+    res.render("pages/list", { productos });
+  }); */
+
+  router.get("/", async (req, res) => {
+    const productos = await nuevo.getAll();
+    res.status(200).json(productos);
+  });
+
+  router.get("/:id", async (req, res) => {
+    const producto = await nuevo.getById(req.params.id);
+    producto
+      ? res.status(200).json(producto)
+      : res
+          .status(400)
+          .json({ error: "Ocurrió un error al encontrar el producto." });
+  });
+
+  /* RENDERIZAR PAGINA DE CREACION DE PRODUCTOS
+  router.get("/crear", async (req, res) => {
+    res.render("pages/form", {});
+  }); */
+
+  router.post("/", async (req, res) => {
+    const { body } = req;
+    (await nuevo.save(body, Producto(body)))
+      ? res.status(200).send({ success: "Producto actualizado." })
+      : res
+          .status(404)
+          .send({ error: "Ocurrió un error al encontrar el producto." });
+    /* res.redirect("/crear"); */
+  });
+
+  router.put("/:id", async (req, res) => {
+    const productUpdated = await nuevo.updateById(req.params.id, req.body);
+    if(Producto(req.body)){
+      productUpdated
+        ? res.status(200).send({ success: "Producto actualizado." })
+        : res
+            .status(404)
+            .send({ error: "Ocurrió un error al encontrar el producto." });
+        return
+    }
+    res
+      .status(404)
+      .send({ error: "Ocurrió un error al actualizar el producto." });
+  });
+
+  router.delete("/:id", async (req, res) => {
+    const productDeleted = await nuevo.deleteById(Number(req.params.id));
+    productDeleted
+      ? res.status(200).send({ success: "Producto eliminado." })
+      : res
+          .status(404)
+          .send({ error: "Ocurrió un error al encontrar el producto." });
+  });
+
+  router.get("/*", async (req, res) => {
+    res.status(404).json({
+      error: -2,
+      descripcion: `ruta ${req.originalUrl} método ${req.method} no implementada`,
+    });
+  });
+
+  return router;
+};
